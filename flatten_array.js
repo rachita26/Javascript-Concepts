9b807adabd7094ad908bc0dd523e6a89fe0c83c4
Rachita Choudhury <rachitachoudhury26@gmail.com>
2022-03-02 18:25:53 +0530

    Create flatten_array.js

---
+++ flatten_array.js
@@ -0,0 +1,23 @@
+const arr = [
+  [1, 2],
+  [3, 4],
+  [5, 6, [7, 8]],
+  [9, 10],
+];
+
+console.log("Array.flat", arr.flat(2));
+//depth 2
+
+console.log("Concat", [].concat(...arr));
+
+function customFlat(arr, depth = 1) {
+  let result = [];
+  arr.forEach((ar) => {
+    if (Array.isArray(ar) && depth > 0) {
+      result.push(...customFlat(ar, depth - 1));
+    } else result.push(ar);
+  });
+  return result;
+}
+
+console.log(customFlat(arr, 2));
